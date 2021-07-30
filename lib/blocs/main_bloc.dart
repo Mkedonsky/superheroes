35f30c65cc99102cc4892a2fe4692ee2a51424b8
alexander <Mkedonsky>
2021-07-30 14:50:56 +0300

    работа с API и сетью

--- lib/blocs/main_bloc.dart
+++ lib/blocs/main_bloc.dart
@@ -4,7 +4,7 @@ import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:http/http.dart' as http;
-import 'package:superheroes/exception%20/api_exception.dart';
+import 'package:superheroes/exception/api_exception.dart';
 import 'package:superheroes/model/superhero.dart';
 
 class MainBloc {
